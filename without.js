f2f1ddd278e52404c2c9d202839dfb069fca1a61
Tamara Tell <2telltamara@gmail.com>
2023-05-02 19:30:58 -0400

    add without.js with function implementation

---
+++ without.js
@@ -0,0 +1,19 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+//FUNCTION IMPLEMENTATION
+const without = (array, itemsToRemove) => {
+  let editedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== itemsToRemove[i]) {
+      editedArray.push(array[i]);
+    }
+  }
+
+  return editedArray;
+};
+
+
+//TEST
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
