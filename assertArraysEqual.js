5e1a26cd45c80586d8b4140a24bbbdfc3cd619ed
Tamara Tell <2telltamara@gmail.com>
2023-05-02 19:32:40 -0400

    export assertArraysEqual and eqArrays to without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,6 +10,8 @@ const assertArraysEqual = (array1, array2) => {
 
 //TEST
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-assertArraysEqual([1, 1, 1], [1, 1, 1]); // => should PASS
-assertArraysEqual([1, "1"], [1, 1]); // => should FAIL
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+// assertArraysEqual([1, 1, 1], [1, 1, 1]); // => should PASS
+// assertArraysEqual([1, "1"], [1, 1]); // => should FAIL
+
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -18,16 +18,16 @@ const eqArrays = (array1, array2) => {
 
 
 //TEST
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 //ASSERTION TESTS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 module.exports = eqArrays;
