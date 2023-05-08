7c01675d0d757f383e8eff5635a9ecdb58203f0a
Tamara Tell <2telltamara@gmail.com>
2023-05-08 15:28:26 -0400

    add mocha and chai tests to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,14 @@
-const assertEqual = require('../assertEqual');
 const getTail = require('../tail');
+const assert = require("chai").assert;
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-getTail(words);
-assertEqual(words.length, 3); 
+
+describe("the function getTail", () => {
+  it("returns all elements of the array except element 0", () => {
+    assert.deepEqual(getTail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("does not modify the length of the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    getTail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
