2227e2e5cfb295c74d32bc0759998860e8f46f36
Tamara Tell <2telltamara@gmail.com>
2023-05-08 15:17:38 -0400

    add chai and mocha tests to headTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -6,3 +6,4 @@ const assertArraysEqual = require("../assertArraysEqual");
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
 assertArraysEqual([1, 1, 1], [1, 1, 1]); // => should PASS
 assertArraysEqual([1, "1"], [1, 1]); // => should FAIL
+
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,17 @@
-const assertEqual = require('../assertEqual');
 const getHead = require('../head');
+const assert = require("chai").assert;
 
-//TEST CODE
-assertEqual(getHead([5, 6, 7]), 5);
-assertEqual(getHead(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(getHead([]), undefined);
-assertEqual(getHead([5]), 5);
+describe("the function getHead", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(getHead([5, 6, 7]), 5);
+  });
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(getHead(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it('returns undefined for []', () => {
+    assert.strictEqual(getHead([]), undefined);
+  });
+  it('returns [5] for [5]', () => {
+    assert.strictEqual(getHead([5]), 5);
+  });
+});
