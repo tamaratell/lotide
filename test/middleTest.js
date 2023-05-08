bc72c349ff5053f48ef710d2120cf13fc06ba3f2
Tamara Tell <2telltamara@gmail.com>
2023-05-08 15:25:11 -0400

    add mocha and chai tests to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const getMiddle = require('../middle');
+const assert = require("chai").assert;
 
-//TEST
-//For arrays with one or two elements, there is no middle. Return an empty array.
-assertArraysEqual(getMiddle([1]), []); // => []
-assertArraysEqual(getMiddle([1, 2]), []); // => []
 
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-assertArraysEqual(getMiddle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(getMiddle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("the function getMiddle", () => {
+  it("returns an empty array for arrays with one element", () => {
+    assert.deepEqual(getMiddle([1]), []);
+  });
+  it("returns an empty array for arrays with two elements", () => {
+    assert.deepEqual(getMiddle([1, 2]), []);
+  });
+  it('returns an array containing a single middle element for arrays with odd numbers of elements', () => {
+    assert.deepEqual(getMiddle([1, 2, 3]), [2]);
+  });
+  it('returns an array containing the two middle element for arrays with even numbers of elements', () => {
+    assert.deepEqual(getMiddle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
