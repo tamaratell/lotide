826e16ca747472a6906e31efb553bca89395fa4f
Tamara Tell <2telltamara@gmail.com>
2023-05-08 15:06:59 -0400

    add middleTest.js with refactored test code from middle.js

--- middle.js
+++ middle.js
@@ -1,6 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
-
 //FUNCTION IMPLEMENTATION
 const getMiddle = (array) => {
   let middle = [];
@@ -20,16 +17,5 @@ const getMiddle = (array) => {
 };
 
 
+module.exports = getMiddle;
 
-//TEST
-//For arrays with one or two elements, there is no middle. Return an empty array.
-assertArraysEqual(getMiddle([1]), []); // => []
-assertArraysEqual(getMiddle([1, 2]), []); // => []
-
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-assertArraysEqual(getMiddle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(getMiddle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
